c83eec77ed614dc11851bac2bee0407be8d7aad4
TheaSlater <theodora.slater@nottingham.ac.uk>
2025-07-30 13:34:31 +0100

    Refine data reading and saving process in Spectrophotometer_Analyse.py

--- Spectrophotometer_Analyse.py
+++ Spectrophotometer_Analyse.py
@@ -23,8 +23,20 @@ if not file_path.endswith('.ods'):
 
 try:
     # Read into pandas DataFrame
-    data = pd.read_excel(file_path, engine='odf')
+    data = pd.read_excel(file_path, header=1, index_col=0, engine='odf')
 except Exception as e:
     print(f"Error: Failed to read the file. Details: {e}")
     sys.exit(1)
 
+# Only looking at the second reading on each sid eof the arm as those are the only complete data sets
+filtered_data = data[data.index.str.endswith('2')]
+
+print(filtered_data)
+# Save the DataFrame as a CSV file in the same directory
+csv_file_path = os.path.splitext(file_path)[0] + '.csv'
+try:
+    filtered_data.to_csv(csv_file_path)
+    print(f"Data successfully saved to '{csv_file_path}'.")
+except Exception as e:
+    print(f"Error: Failed to save the file as CSV. Details: {e}")
+    sys.exit(1)
